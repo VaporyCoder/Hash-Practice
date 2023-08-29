d82af9ec501450c5e136a530aaf199cdb631c24b
Jon <jlara2019@gmail.comgit config --global user.email jlara2019@gmail.compwdgit commit>
2023-08-29 14:05:44 -0500

    in progress

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello world!");
+const state = {
+  wizardList: [],
+  character: null,
+};
+
+async function getWizardList() {
+  const info = await fetch("https://hp-api.onrender.com/api/characters");
+  const wizardData = await info.json();
+  console.log(wizardData);
+  state.wizardList = wizardData;
+  console.log(state);
+}
+
+async function render() {
+    await getWizardList();
+}
+
+render();
