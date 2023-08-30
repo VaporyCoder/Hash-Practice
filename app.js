b87c4e5db0196b1b6d9336d64654ea5bb0ec47c9
Jonathan Lara <vaporycoder@Jonathans-MBP.attlocal.net>
2023-08-29 19:30:29 -0500

    need help

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
+window.addEventListener("hashchange", () => {
+  selectWizard();
+});
+
 const state = {
   wizardList: [],
-  character: null,
+  character: null
 };
+
+function selectWizard() {
+  getEventHash();
+  renderWizardDetails();
+}
+
+function getEventHash() {
+  const name = window.location.hash.slice(1);
+  const singleWizard = state.wizardList.find((character) => {
+    return character.name === name;
+  });
+
+  state.character = singleWizard;
+  console.log(state);
+}
+
+console.log(getEventHash());
+
+function renderWizardDetails() {
+  if (state.character) {
+    getSingleWizard();
+  }
+}
+
+const allWizardsDiv = document.querySelector(".listDiv");
+const characterDiv = document.querySelector(".charDiv");
+
+async function getSingleWizard() {
+  const wizardData = await fetch(
+    `https://hp-api.onrender.com/api/characters/${state.character.name}`
+  );
+  const singleWizardData = await wizardData.json();
+  state.character = singleWizardData;
+  console.log(state);
+}
+
+function renderWizardList() {
+  const allWizards = state.wizardList.map((wizard) => {
+    return `<div> <a href=#${wizard.name}>${wizard.name}</a> </div>`;
+  });
+  allWizardsDiv.innerHTML = allWizards.join("");
+}
 
 async function getWizardList() {
   const info = await fetch("https://hp-api.onrender.com/api/characters");
@@ -12,7 +58,9 @@ async function getWizardList() {
 }
 
 async function render() {
-    await getWizardList();
+  await getWizardList();
+  renderWizardList();
+  selectWizard();
 }
 
 render();
